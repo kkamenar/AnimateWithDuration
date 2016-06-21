cbc161216331c982c03fa5818a9b766c31a1d7d7
Kristin Kamenar <kkamenar@kristins-macbook-air-5.local>
2016-06-21 13:30:04 -0500

    Make text appear and disappear with animateWithDuration. Modify alpha transparency level.

--- AnimateWithDuration/ViewController.swift
+++ AnimateWithDuration/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //set label info programatically
+        myLabel.text = "Hello World!"
+        //myLabel.alpha = 0
     }
 
+    @IBAction func screenTapped(sender: UITapGestureRecognizer) {
+        
+        //print("tap")
+        
+        //set up 2 second animation
+        
+        //if visible text, make invisible
+        if myLabel.alpha == 1.0
+        {
+        
+            UIView.animateWithDuration(2.0)
+            {
+                self.myLabel.alpha = 0.0
+            }
+        }
+        
+        //if text is invisible, make visible
+        else if myLabel.alpha == 0.0
+        {
+            UIView.animateWithDuration(2.0)
+            {
+                self.myLabel.alpha = 1.0
+            }
+        }
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
